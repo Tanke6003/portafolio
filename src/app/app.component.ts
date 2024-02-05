05d10dac863e1ed7bbe10ad07476dd3993f9ff33
Tanke6003 <Ruben.Farias.1999@Outlook.es>
2024-02-04 18:28:12 -0600

    home struct introduce

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'portafolio';
+  title = 'Ruben Farias - Full Stack Developer';
 }
